f2f5db4e10b258b71ef6597041f9c0a64e4e6c29
Wes Huang <ops@netdiversity.ca>
2025-09-05 18:24:12 -0600

    Added route

--- src/core/route-map.js
+++ src/core/route-map.js
@@ -1,9 +1,11 @@
 // src/core/route-map.js
 import { pp_detail, ppDetailsRoute } from '@/pages/pp_detail';
 import { pp_history, ppHistoryRoute } from '@/pages/pp_history';
+import { pp_schedule, ppScheduleRoute } from '@/pages/pp_schedule';
 
 export const routeMap = {
     [pp_detail.key]: (params) => ppDetailsRoute(params),
-    [pp_history.key]: () => ppHistoryRoute()
+    [pp_history.key]: () => ppHistoryRoute(),
+    [pp_schedule.key]: () => ppScheduleRoute()
     // Add more as needed
 };
