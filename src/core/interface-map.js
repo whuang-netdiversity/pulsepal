5d6063506093160bbfb73895253810384fe55b1d
Wes Huang <ops@netdiversity.ca>
2025-09-05 18:24:39 -0600

    Updated repeater decorators

--- src/core/interface-map.js
+++ src/core/interface-map.js
@@ -1,12 +1,14 @@
 // /src/core/interface-map.js
 import { updateRepeater } from '@/app/ui';
 import { pp_detail } from '@/pages/pp_detail';
-import { updateClassification, updateHistory } from '@/app/ui';
+import { updateClassification, updateHistory, updateSchedule } from '@/app/ui';
 import { pp_history } from '@/pages/pp_history';
+import { pp_schedule } from '@/pages/pp_schedule';
 
 export const interfaceMap = {
     [pp_detail.prop.repeater]: (params) => updateRepeater(pp_detail.prop,
         updateClassification(pp_detail.prop.repeater, params), true),
-    [pp_history.prop.repeater]: () => updateRepeater(pp_history.prop, updateHistory(), true)
+    [pp_history.prop.repeater]: () => updateRepeater(pp_history.prop, updateHistory(), true),
+    [pp_schedule.prop.repeater]: () => updateRepeater(pp_schedule.prop, updateSchedule(), true)
     // Add more as needed
 };
